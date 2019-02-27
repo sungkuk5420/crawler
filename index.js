b5d10dae7a720914cc8aa1020b312a6cd3580ac8
Sungkuk Kim <sungkuk5420@gmail.com>
2019-02-27 13:23:10 +0900

    xlsx parse

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const parse = require('csv-parse/lib/sync');
 const fs = require('fs');
+const xlsx = require('xlsx');
 
-const csv = fs.readFileSync('csv/data.csv');
-const records = parse(csv.toString('utf-8'));
-records.forEach((r, i) => {
+//parse csv file
+// const csv = fs.readFileSync('csv/data.csv');
+// const records = parse(csv.toString('utf-8'));
+// records.forEach((r, i) => {
+//     console.log(i, r);
+// });
+
+//parse csv file end point
+
+//parse xlsx file
+const workbook = xlsx.readFile('xlsx/data.xlsx');
+console.log(Object.keys(workbook.Sheets)); // TODO: workbook.SheetNames
+const ws = workbook.Sheets.영화목록;
+const records = xlsx.utils.sheet_to_json(ws); // TODO: 강좌에서 header 옵션 보여주기
+for (const [i, r] of records.entries()) {
     console.log(i, r);
-});
+}
